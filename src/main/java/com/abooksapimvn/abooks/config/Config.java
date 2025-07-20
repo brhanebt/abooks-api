509fc122d7abd258aa55edeb097151aaf7ce733f
BT <brhane.teka3@gmail.com>
2025-07-20 14:49:30 -0500

    bks-ang-v02

--- src/main/java/com/abooksapimvn/abooks/config/Config.java
+++ src/main/java/com/abooksapimvn/abooks/config/Config.java
@@ -3,6 +3,8 @@ package com.abooksapimvn.abooks.config;
 import org.hibernate.boot.model.naming.PhysicalNamingStrategyStandardImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class Config {
@@ -14,4 +16,15 @@ public class Config {
     // public ImplicitNamingStrategy implicit(){
     //     return new ImplicitNamingStrategyLegacyJpaImpl();
     // }
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer(){
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/v1/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
 }
